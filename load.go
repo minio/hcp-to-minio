b3f749e6ddcd1c5ef37c3bbc59083b0f8dd6c6b4
Poorna Krishnamoorthy <poorna@minio.io>
2021-04-26 17:49:03 -0700

    Add timestamp to output file names
    Signed-off-by: Poorna Krishnamoorthy <poorna@minio.io>

--- load.go
+++ load.go
@@ -9,6 +9,7 @@ import (
 	"runtime"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	miniogo "github.com/minio/minio-go/v7"
 )
@@ -111,7 +112,7 @@ func (m *migrateState) init(ctx context.Context) {
 		m.addWorker(ctx)
 	}
 	go func() {
-		f, err := os.OpenFile(path.Join(dirPath, failMigFile), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+		f, err := os.OpenFile(path.Join(dirPath, failMigFile+time.Now().Format(".01-02-2006-15-04-05")), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 		if err != nil {
 			logDMsg("could not create + migration_fails.txt", err)
 			return
@@ -137,7 +138,7 @@ func (m *migrateState) init(ctx context.Context) {
 		}
 	}()
 	go func() {
-		f, err := os.OpenFile(path.Join(dirPath, logMigFile), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+		f, err := os.OpenFile(path.Join(dirPath, logMigFile+time.Now().Format(".01-02-2006-15-04-05")), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 		if err != nil {
 			logDMsg("could not create + migration_log.txt", err)
 			return
